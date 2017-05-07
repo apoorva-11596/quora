62e079cab46cffb2c2fbf7ab175ac002723876ed
apoorva-11596 <apoorva.11596@gmail.com>
2017-05-07 05:37:58 +0530

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_user
 
-
+  def after_sign_in_path_for(resource)
+    '/questions'
+  end
 
    def user_image_url(user)
     # byebug
